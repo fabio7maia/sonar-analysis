ecb01ddd41ced75fc8f9cfa3ba31224c5c9c858d
Fábio Maia <fabio.maia@itsector.pt>
2021-05-05 15:56:09 +0100

    Test new analisys

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const a = () => {
+    console.log("test");
+  };
+
+  const b = () => {
+    console.log("test");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
